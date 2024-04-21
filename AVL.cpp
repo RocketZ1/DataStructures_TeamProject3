a1af2864b5872d5887d4185bb0211e343d3ec4f5
Zane Wolfe <zwolfe4232@floridapoly.edu>
2024-04-21 15:09:23 -0400

    Reworked entire AVL tree to fix "most" bugs

--- AVL.cpp
+++ AVL.cpp
@@ -38,19 +38,51 @@ public:
     }
 
     int getBalanceFactor() {
-        return this->left->getHeight() - this->right->getHeight();
+        int leftHeight = 0, rightHeight = 0;
+        this->left == nullptr ? 0 : leftHeight = left->getHeight();
+        this->right == nullptr ? 0 : rightHeight = right->getHeight();
+        return leftHeight - rightHeight;
+    }
+
+    AVLNode* getLeft(){
+        return this->left;
+    }
+
+    AVLNode* getRight(){
+        return this->right;
+    }
+
+    Airport getAirport(){
+        return this->airport;
     }
 
     AVLNode* rightRotate() {
         AVLNode* newRoot = this->left;
-        AVLNode* temp = newRoot->right;
+        AVLNode* temp = this->right;
 
         newRoot->right = this;
         this->left = temp;
 
-        this->height = 1 + max(this->left->getHeight(), this->right->getHeight());
-        newRoot->height = 1 + max(newRoot->left->getHeight(), newRoot->right->getHeight());
-
+        // Fixed NULL segmentation error height code
+        int leftHeight = 0;
+        int rightHeight = 0;
+        if(this->left != nullptr){
+            leftHeight = this->left->getHeight();
+        }
+        if(this->right != nullptr){
+            rightHeight = this->right->getHeight();
+        }
+        this->height = 1 + max(leftHeight, rightHeight);
+        //this->height = 1 + max(this->left->getHeight(), this->right->getHeight());
+        // reset both height variables to 0
+        leftHeight = rightHeight = 0;
+        if(newRoot->left != nullptr){
+            leftHeight = newRoot->left->getHeight();
+        }
+        if(newRoot->right != nullptr){
+            rightHeight = newRoot->right->getHeight();
+        }
+        newRoot->height = 1 + max(leftHeight, rightHeight);
         return newRoot;
     }
 
@@ -72,32 +104,48 @@ public:
         origin->airport.distances.push_back(distance);
         origin->airport.costs.push_back(cost);
     }
-    AVLNode* insertAirport(Airport airport) {
-        AVLNode* newNode = nullptr;
+
+    AVLNode* insertAirport(Airport &airport) {
+        if(this->airport.code.empty()){
+            this->airport = airport;
+            this->height = 1;
+            return this;
+        }
+
+        AVLNode* newNode;
         if (airport.code < this->airport.code) {
             if (this->left == nullptr) {
                 this->left = new AVLNode(airport);
                 newNode = this->left;
             } else {
-                this->left->insertAirport(airport);
+                newNode = this->left->insertAirport(airport);
             }
         } else if (airport.code > this->airport.code) {
             if (this->right == nullptr) {
                 this->right = new AVLNode(airport);
                 newNode = this->right;
             } else {
-                this->right->insertAirport(airport);
+                newNode = this->right->insertAirport(airport);
             }
         }
         else {
             return this;
         }
-        this->height = 1 + max(this->left->getHeight(), this->right->getHeight());
+        int leftHeight = 0;
+        int rightHeight = 0;
+        if(this->left != nullptr){
+            leftHeight = this->left->getHeight();
+        }
+        if(this->right != nullptr){
+            rightHeight = this->right->getHeight();
+        }
+        this->height = 1 + max(leftHeight, rightHeight);
 
-        int balanceFactor = this->getBalanceFactor();
+        int balanceFactor = getBalanceFactor();
 
         if (balanceFactor > 1) {
             this->rightRotate();
         }
+        return newNode;
     }
 };
